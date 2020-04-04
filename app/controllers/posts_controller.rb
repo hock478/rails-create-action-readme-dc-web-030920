ffa1485116115bbfec497bb5cc3391f5f171c87d
Dawit Gizaw <gizawdawit123.com>
2020-04-04 12:35:44 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.create(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
+  end
+
   def new
     @post = Post.new
   end
